c5366f5573e4d193f129d792eb5da383a95bd337
Wayez <c.wayez3793@gmail.com>
2016-03-03 03:17:03 +0000

    fixing stuff up

--- Go/helloWorld.go
+++ Go/helloWorld.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
-
-  fmt.Printf("")
+	var i int
+	var n int
+	var yes bool = true
+	const e float64 = 2.718
+	i = 9
+	n = 78
+  fmt.Printf("hello, world!\n")
+  fmt.Printf("%t\n", yes)
+  fmt.Printf("%s\n", "turtle")
+  fmt.Printf("%d\n", i + n)
+  fmt.Printf("%.2f\n", e)
 }
+
+
+
--- hw03/animate.js
+++ hw03/animate.js
@@ -1,7 +1,8 @@
-var c = document.getElementById("playground");
-var b = document.getElementById("clear");
-var s = document.getElementById("start");
-var ctx = c.getContext("2d");
+var c = document.getElementByID("playground");
+var d = document.getElementByID("dot");
+var v = document.getElementByID("dvd");
+var s = document.getElementByID("stop");
+var cx = c.getContext("2d");
 
 //Variables to keep track of radius
 var r;
