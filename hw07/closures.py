b5cb845d7c600b5476b5318ac63a13200545c5fe
Wayez Chowdhury <c.wayez3793@gmail.com>
2016-04-04 09:30:34 -0400

    lab07 done

---
+++ hw07/closures.py
@@ -0,0 +1,48 @@
+def inc(x):
+    return x + 1
+
+f = inc
+
+#print f(10)
+
+def h(x):
+    return lambda y: x + y
+
+#print h(1)
+#print h(2)
+#print h(1)(3)
+#print h(2)(5)
+
+b=h(1)
+
+#print b
+
+def f(x):
+    def g(y):
+        return x + y
+    return g
+
+#print f(1)
+#print f(2)
+#print f(1)(3)
+#print f(2)(5)
+
+c=h(1)
+
+#print(c)
+#print c(3)
+
+def repeat(text):
+    def r(num):
+        ans = ""
+        for x in range(num):
+            ans += text
+        return ans
+    return r
+
+r1 = repeat('hello')
+r2 = repeat('goodbye')
+
+print r1(2)
+print r2(2)
+print repeat('cool')(3)
