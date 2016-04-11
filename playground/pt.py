731b2682660ab3d42de7b82a898c337392f426e2
Wayez Chowdhury <c.wayez3793@gmail.com>
2016-04-11 09:40:20 -0400

    quicksort done

--- playground/pt.py
+++ playground/pt.py
@@ -32,3 +32,20 @@ print pt2(20)
 #   *all values > p are OH
 #   *PIVOT is at right place
 #3. qsort(LH) + p + qsort(OH)
+
+from random import randint
+
+test = [randint(0, 1000) for x in range(10)]
+#test = [3, 23, 5,3 , 354, 2, 5435,5, 234, 2, 86, 64, 9]
+def qsort(List):
+    if len(List) <= 1:
+        return List
+    index = randint(0, len(List) - 1)
+    pivot = List[index]
+    lh = [ x for x in List if x < pivot]
+    oh = [ x for x in List if x > pivot]
+    c = [ x for x in List if x == pivot]
+    return qsort(lh) + c + qsort(oh)
+
+print test
+print qsort(test)
